13c61a3de1cbe75ad827dd4f2dcf672ebd04a5fd
fujimotoAr <theofilus.arkhi@mail.ugm.ac.id>
2021-09-30 21:40:22 +0700

    ganti login,signupSensei ke login,signupTutor

--- senseikuBackend/senseikuApp/urls.py
+++ senseikuBackend/senseikuApp/urls.py
@@ -5,10 +5,10 @@ from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-    path('loginSensei/', views.loginTutor, name='loginTutor'),
+    path('loginTutor/', views.loginTutor, name='loginTutor'),
     path('loginStudent/', views.loginStudent, name='loginStudent'),
     path('signupSensei/',views.signupTutor, name='signupTutor'),
-    path('signupStudent/',views.signupStudent, name='signupStudent'),
+    path('signupTutor/',views.signupStudent, name='signupStudent'),
     path('newCourse/',viewsProduct.getNewCourse, name='getNewCourse'),
     path('addCourse/',viewsProduct.addCourse, name='addCourse'),
 
