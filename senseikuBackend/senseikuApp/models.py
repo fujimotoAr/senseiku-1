0801f401a6e1c0c77326e6c747308c05b7f11ee6
fujimotoAr <theofilus.arkhi@mail.ugm.ac.id>
2021-10-06 21:52:54 +0700

    belum test

--- senseikuBackend/senseikuApp/models.py
+++ senseikuBackend/senseikuApp/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 
-# tutor = Group.objects.create(name='tutor')
-# student = Group.objects.create(name='student')
 
 class Course(models.Model):
     id = models.AutoField(primary_key=True,unique=True)
@@ -12,3 +10,18 @@ class Course(models.Model):
     description = models.CharField(max_length=1000)
     pricing = models.IntegerField()
     tutor_id = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    
+class Schedule(models.Model):
+    id = models.AutoField(primary_key=True,unique=True)
+    course_id = models.ForeignKey(Course,on_delete=models.CASCADE)
+    day = models.CharField(max_length=100)
+    hour = models.TimeField()
+
+class Review(models.Model):
+    id = models.AutoField(primary_key=True,unique=True)
+    course_id = models.ForeignKey(Course,on_delete=models.CASCADE)
+    student_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    review = models.CharField(max_length=1000)
+    rating = models.FloatField(default=0.0)
+
--- senseikuBackend/senseikuApp/viewsProduct.py
+++ senseikuBackend/senseikuApp/viewsProduct.py
@@ -1,6 +1,6 @@
 from json.decoder import JSONDecodeError
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from .models import Course
+from .models import Course, Schedule
 from django.core import serializers
 from django.http import HttpResponse,JsonResponse
 from django.db import IntegrityError
@@ -18,15 +18,14 @@ csrf_exempt
 def addCourse(request):
     data=json.loads(request.body.decode('utf-8'))
     courseDict={
-        "id":data['id'],
         "course_name":data['nama'],
         "description":data['deskripsi'],
         "pricing":data['harga'],
-        "tutor_id":data['tutor_id']
+        "tutor_id":data['tutor_id'],
+        "message":"Add course berhasil"
     }
     try:
         Course.objects.create(
-            id=courseDict['id'],
             course_name=courseDict['course_name'],
             description=courseDict['description'],
             pricing=courseDict['pricing'],
@@ -34,4 +33,30 @@ def addCourse(request):
         )
         return JsonResponse(courseDict)
     except IntegrityError:
+        courseDict.update({
+            "message":"Add course gagal"
+        })
         return JsonResponse(courseDict, status=404)
+
+csrf_exempt
+def addSchedule(request):
+    data=json.loads(request.body.decode('utf-8'))
+    
+    scheduleDict={
+        "course_id":data['course_id'],
+        "day":data['day'],
+        "hour":data['hour'],
+        "message":"Add schedule berhasil"
+    }
+    try:
+        Schedule.objects.create(
+            course_id=scheduleDict['course_id'],
+            day=scheduleDict['day'],
+            hour=scheduleDict['hour'],
+        )
+        return JsonResponse(scheduleDict)
+    except IntegrityError:
+        scheduleDict.update({
+            "message":"Add schedule gagal"
+        })
+        return JsonResponse(scheduleDict, status=404)
